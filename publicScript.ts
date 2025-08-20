81de541109304c24680397f3e5c6f9ed2fc16c65
Camila Pierce <camilaepierce@gmail.com>
2025-08-20 18:08:30 -0400

    added photos, google font

--- publicScript.ts
+++ publicScript.ts
@@ -1,12 +1,34 @@
-window.onscroll = function() {myFunction()};
 
-var header = document.getElementById("navigation");
-var sticky = header.offsetTop;
 
-function myFunction() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+function stickNavigation() {
+  var header = document.getElementById("navigation");
+  var headerHeight = header?.offsetTop;
+  if (header && headerHeight) {
+    if (window.pageYOffset > headerHeight) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
   }
 }
+
+// function replace_image(imgID: string, newImage: string) {
+//   var old_image = document.getElementById(imgID)
+//   if (old_image) {
+  //     old_image.style = CSS
+  //   }
+// }
+
+window.onscroll = function() {stickNavigation()};
+window.onscroll = function () {
+  var testElement = document.getElementById("page_spacer")
+  testElement?.setAttribute("text", "hello world");
+}
+var button = document.getElementById("button")
+
+// public async myFunction() {
+
+// }
+// function buttonClicking() {
+
+// }
